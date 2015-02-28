54faa98014ac94c79b45bf4686946b87c0be9f3f
weinianjie <weinianjie@gmail.com>
2015-02-28 16:27:59 +0800

    finish the map and frame

--- stock.php
+++ stock.php
@@ -7,37 +7,60 @@
     <link rel="stylesheet" type="text/css" href="style.css" />
     <script type="text/javascript">
 			$(document).ready(function(){
-				var stockId = "sz000333";
-				var url = "http://hq.sinajs.cn/list=" + stockId;
-				$.ajax({url: url, dataType: "script", cache:true, success: function(){
-						eval("var sinaArr = hq_str_" + stockId + ".split(',')");
-						if(sinaArr && sinaArr.length > 0) {
-							var summary_html = "<span>" + sinaArr[0] + "(" + stockId + ")</span>";
-							summary_html += "<span>昨收：" + sinaArr[2] + "</span>";
-							summary_html += "<span>今开：" + sinaArr[1] + "</span>";
-							summary_html += "<span>当前：" + sinaArr[3] + "</span>";
-							summary_html += "<span>最高：" + sinaArr[4] + "</span>";
-							summary_html += "<span>最低：" + sinaArr[5] + "</span>";
-							$(".stock_summary td").html(summary_html);
-						}						
-					}
-				});		
-
-				
+				var stockId = "<?php echo $_GET["stockId"]; ?>";
+				if(stockId){
+					var url = "http://hq.sinajs.cn/list=" + stockId;
+					$.ajax({url: url, dataType: "script", cache:true, success: function(){
+							eval("var sinaArr = hq_str_" + stockId + ".split(',')");
+							if(sinaArr && sinaArr.length > 0) {
+								var summary_html = "<span>" + sinaArr[0] + "(" + stockId + ")</span>";
+								summary_html += "<span>昨收：" + sinaArr[2] + "</span>";
+								summary_html += "<span>今开：" + sinaArr[1] + "</span>";
+								summary_html += "<span>当前：" + sinaArr[3] + "</span>";
+								summary_html += "<span>最高：" + sinaArr[4] + "</span>";
+								summary_html += "<span>最低：" + sinaArr[5] + "</span>";
+								$(".stock_summary td").html(summary_html);
+							}						
+						}
+					});
+				}
 			});
 		</script>
   </head>
   <body>
+  	<?php
+			if(isset($_GET["stockId"])){
+				$stockId = $_GET["stockId"];
+				$stockCode = substr($stockId,-6)
+		?>
+		
+		
   	<table class="stock" border=1>
   		<tr class="stock_summary">
-  			<td colspan="2">
-  				<span></span>
+  			<td colspan="3">
   			</td>
   		</tr>
-  		<tr class="stock_graph">
-  			<td>1</td>
-  			<td>2</td>
+  		<tr class="stock_detail">
+  			<td class="map">
+  				<img class="kmap" src="http://image.sinajs.cn/newchart/min/n/<?php echo $stockId; ?>.gif" />
+  				<img class="kmap" src="http://image.sinajs.cn/newchart/daily/n/<?php echo $stockId; ?>.gif" />
+  			</td>
+  			<td class="deal">
+  				
+  			</td>
+  			<td class="other">
+  				<div><a href="http://stockhtm.finance.qq.com/sstock/ggcx/<?php echo $stockCode; ?>.shtml" target="_blank">详细</a></div>
+  			</td>
   		</tr>  		
-  	</table>  	
+  	</table>
+  			
+  			
+		<?php
+			}else{
+		?>
+		stockId is null!
+		<?php
+			}		
+		?>  	
   </body>
 </html>

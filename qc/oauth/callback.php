5469f1bfa4f505bafe748e88e215765759cfd63f
weinianjie <weinianjie@gmail.com>
2015-03-16 17:59:34 +0800

    4

--- qc/oauth/callback.php
+++ qc/oauth/callback.php
@@ -1,10 +1,20 @@
 <?php
 require_once("../API/qqConnectAPI.php");
 $qc = new QC();
+
+
 if($qc->is_login()) {
 	var_dump($qc->get_user_info());
 }else{
 	echo "no login";
 }
+
+
+echo "|";
+
+echo empty($_SESSION['QC_userData']) ? "null":"not null";
+
+
+
 //echo $qc->qq_callback();
 //echo $qc->get_openid();
